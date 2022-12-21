7095d59dfed067f071081f06227251374214dd73
Плетнёв Кирилл <Ropkap911@gmail.com>
2022-12-22 01:59:49 +0300

    Hw 11: 2 tasks

---
+++ Lab-11-frontend/src/components/main/Index.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="articles">
+        <Post v-for="post in posts" :post="post" :key="post.id"/>
+    </div>
+</template>
+
+<script>
+import Post from "../page/Post";
+
+export default {
+    name: "Index",
+    props: ["posts"],
+    components: {
+        Post
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ Lab-11-frontend/src/components/page/Post.vue
@@ -0,0 +1,30 @@
+<template>
+    <article>
+        <div class="title">{{ post.title }}</div>
+        <div class="information">By {{ post.user.name }}</div>
+        <div class="body">{{ post.text }}</div>
+        <div class="footer">
+            <div class="left">
+                <img src="../../assets/img/voteup.png" title="Vote Up" alt="Vote Up"/>
+                <span class="positive-score">{{ post.id * 10 }}</span>
+                <img src="../../assets/img/votedown.png" title="Vote Down" alt="Vote Down"/>
+            </div>
+            <div class="right">
+                <img src="../../assets/img/date_16x16.png" title="Publish Time" alt="Publish Time"/>
+                <img src="../../assets/img/comments_16x16.png" title="Comments" alt="Comments"/>
+                <a href="#">{{ post.id}}</a>
+            </div>
+        </div>
+    </article>
+</template>
+
+<script>
+export default {
+    name: "Post",
+    props: ["post"]
+}
+</script>
+
+<style scoped>
+
+</style>
